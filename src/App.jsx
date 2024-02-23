752569c436edf29b65a1700a7fb71ff2eda8e60e
harshil-kakdiya <harshilpatel0334@gamil.com>
2024-02-23 13:55:19 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {  HashRouter, Route, Routes } from "react-router-dom";
 import Loading from "./pages/Loading"
 import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route exact path="/" element={ <Loading />} />
         <Route exact path="/home" element={ <Home />} />
         </Routes>
-    </Router>
+    </HashRouter>
   );
 };
 
